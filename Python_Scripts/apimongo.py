383113bb8b4f3c47edc7a07e40239f5131b34f88
lgendrot <lgendrot@gmail.com>
2016-03-02 12:45:40 -0800

    added update option to storage functions

--- Python_Scripts/apimongo.py
+++ Python_Scripts/apimongo.py
@@ -1,12 +1,13 @@
 import pymongo
 import tweepy
+import time
 from bson.json_util import dumps
 
 
-CONSUMER_KEY = 'PzdjDvz5rnt7tD2ORhaUoLDxL'
-CONSUMER_SECRET = 'VhAXe0IsE68qC81zi2u1GTK6l8cg2cqek2IZVzcxbvfGL2Ia1O'
-ACCESS_KEY = '67676031-O7u9TiKuT3ptxvUGwYgfDPK4y7HvQ7EFb37XhQDRA'
-ACCESS_SECRET = 'cbWjRWTanPkLzlMQJ3QmcnLqWaspPsSCVv251kUAprGRt'
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
@@ -15,13 +16,18 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 
 def search(query, limit=100):
-	tweets = [tweet for tweet in tweepy.Cursor(api.search,
+	if limit > 0:
+		tweets = [tweet for tweet in tweepy.Cursor(api.search,
 																						 q=query, 
 																						 lang="en").items(limit)]
+	else:
+		tweets = [tweet for tweet in tweepy.Cursor(api.search,
+																						 q=query, 
+																						 lang="en").items()]
 	return tweets
 	
 
-def store(tweets):
+def store(tweets, update=False):
 	try:
 		conn = pymongo.MongoClient()
 		print "Connected successfully!"
@@ -33,37 +39,78 @@ def store(tweets):
 
 	for tweet in tweets:
 		try:
-			collection.insert({
-								'id_str': tweet.id_str,
-								'text': tweet.text.encode('utf-8', 'ignore'),
-								'user': tweet.user._json	,
-								'retweet_count': tweet.retweet_count,
-								'favorite_count': tweet.favorite_count,
-								'geo': tweet.geo,
-								'hashtags': tweet.entities['hashtags'],
-								'user_mentions': tweet.entities['user_mentions']
-								})
+			if update:
+				collection.update_one(
+									{'id_str': tweet.id_str},
+									{
+										'$set':	
+											{
+											'id_str': tweet.id_str,
+											'text': tweet.text.encode('utf-8', 'ignore'),
+											'user': tweet.user._json	,
+											'retweet_count': tweet.retweet_count,
+											'favorite_count': tweet.favorite_count,
+											'geo': tweet.geo,
+											'hashtags': tweet.entities['hashtags'],
+											'user_mentions': tweet.entities['user_mentions'],
+											'created_at': tweet.created_at
+											}
+									},
+									upsert=True)
+			else:
+				collection.insert({
+									'id_str': tweet.id_str,
+									'text': tweet.text.encode('utf-8', 'ignore'),
+									'user': tweet.user._json	,
+									'retweet_count': tweet.retweet_count,
+									'favorite_count': tweet.favorite_count,
+									'geo': tweet.geo,
+									'hashtags': tweet.entities['hashtags'],
+									'user_mentions': tweet.entities['user_mentions'],
+									'created_at': tweet.created_at
+									})
 		except pymongo.errors.DuplicateKeyError:
+			print "Duplicate key, skipping: " + tweet.id_str
 			continue
 		except UnicodeEncodeError:
 			print "Unicode Error"
 
 
-queries = ["bank of america", "wells fargo", "verizon", "ATT", "AT&T", "lowes", "home depot"]
+def loop(hours=10):
+	queries = ["bank of america", "wells fargo", "verizon",
+						 "ATT", "AT&T", "lowes", "home depot"]
+	print "Scraping for the next " + str(hours) + " hours"
+	t0 = time.time()
+	time_left = 20*60 + 1
+	while time_left > 3600:
+		completed = []
+		while set(queries) != set(completed):
+				for query in queries:
+					if query not in completed:
+						try:
+							tweets = search(query, limit=0)
+							store(tweets, update=True)
+							completed.append(query)
+						except:
+							continue
+		time_left = 3600*hours - (time.time() - t0)
+		print "There are " + str(time_left/60/60) + " hours left"
+		
 
-completed = []
-done = False
-while done == False:
-	for query in queries:
-		if query not in completed:
-			try:
-				tweets = search(query, limit=2000)
-				store(tweets)
-				completed.append(query)
-				if set(queries) == set(completed):
-					done = True
-			except:
-				continue
 
+
+# def test(seconds=10):
+# 	queries = ['1', 'eleven', 'twent22']
+# 	t0 = time.time()
+# 	time_left = 1
+# 	while time_left > 0:
+# 		completed = []
+# 		while set(queries) != set(completed):
+# 			for query in queries:
+# 				if query not in completed:
+# 					# print "appending: " + str(query)
+# 					completed.append(query)
+# 		time_left = 1*seconds-(time.time()-t0)
+# 		print time_left
 
 
--- Python_Scripts/jsonmongo.py
+++ Python_Scripts/jsonmongo.py
@@ -14,23 +14,43 @@ db = conn.TweetSentiment
 collection = db.timelines
 
 
-def mongo_from_file(filepath):
+def mongo_from_file(filepath, update=False):
 	with open(filepath) as open_file:
 		open_file = open_file.read()
 		jsonfile = ast.literal_eval(open_file)
 		for f in jsonfile:
 			try:
-				collection.insert({
-										'id_str': f['id_str'],
-										'text': f['text'].decode('latin1').encode('utf-8', 'ignore'),
-										'id_str': f['id_str'],
-										'user': f['user'],
-										'retweet_count': f['retweet_count'],
-										'favorite_count': f['favorite_count'],
-										'geo': f['geo'],
-										'hashtags': f['entities']['hashtags'],
-										'user_mentions': f['entities']['user_mentions']
-										})
+				if update:
+					collection.update_one({'id_str': f['id_str']},
+						{
+							'$set': {
+							'id_str': f['id_str'],
+							'text': f['text'].decode('latin1').encode('utf-8', 'ignore'),
+							'id_str': f['id_str'],
+							'user': f['user'],
+							'retweet_count': f['retweet_count'],
+							'favorite_count': f['favorite_count'],
+							'geo': f['geo'],
+							'hashtags': f['entities']['hashtags'],
+							'user_mentions': f['entities']['user_mentions'],
+							'created_at': f['created_at']
+							}
+						},
+						upsert=True)
+				else:
+					collection.insert({
+											'id_str': f['id_str'],
+											'text': f['text'].decode('latin1').encode('utf-8', 'ignore'),
+											'id_str': f['id_str'],
+											'user': f['user'],
+											'retweet_count': f['retweet_count'],
+											'favorite_count': f['favorite_count'],
+											'geo': f['geo'],
+											'hashtags': f['entities']['hashtags'],
+											'user_mentions': f['entities']['user_mentions'],
+											'created_at': f['created_at']
+											})
+					
 			except pymongo.errors.DuplicateKeyError:
 				continue
 			except:
@@ -40,7 +60,7 @@ def store_all_from_file(base_dir):
 	for root, dir, files in os.walk(base_dir):
 		if root is not base_dir:
 			tl_path = root+"/timeline.json"
-			mongo_from_file(tl_path)
+			mongo_from_file(tl_path, update=True)
 
 
 
